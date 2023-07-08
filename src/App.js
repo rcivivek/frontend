58a1996dc2d5997b6ae323eaf88725b18200ee6e
Vivek Goyal <rcivivek41214008@gmail.com>
2023-07-09 04:04:42 +0530

    Routing Implemention and creating project skelton

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
-import { BrowserRouter  as Router}  from 'react-router-dom/cjs/react-router-dom.min';
+import { BrowserRouter  as Router,Route}  from 'react-router-dom/';
+
+import Users from './user/pages/Users';
+import { Redirect } from 'react-router-dom/cjs/react-router-dom.min';
+
 const  App=() =>{
   return <Router>
+    <Route path='/' exact>
+       <Users />
+    </Route>
+    <Redirect to='/'/>
     </Router>
 }
 
