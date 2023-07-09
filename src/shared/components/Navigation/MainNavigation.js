46a113b499fb175dbe0bb89936adafb47775c8e1
Vivek Goyal <rcivivek41214008@gmail.com>
2023-07-09 23:58:23 +0530

    Header and MainNavigation  is completed.

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,23 +1,47 @@
-import React from 'react';
+import React, { useState} from 'react';
 import MainHeader from './MainHeader';
 import './MainNavigation.css';
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 import NavLinks from './NavLinks';
+import SideDrawer from './SideDrawer';
+import Backdrop from '../UIElements/Backdrop';
 
-const MainNavigation= props => {
-    return <MainHeader>
-         <button className='main-navigation__menu-btn'>
-            <span/>
-            <span/>
-            <span/>
-        </button>
-        <h1 className='main-navigation__title'>
-          <Link  to= "/">YourPlaces</Link>
-        </h1>
-        <nav>
-            <NavLinks/>
-        </nav>
-    </MainHeader>
-
-};
-export default MainNavigation;
+const MainNavigation = props => {
+    const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+  
+    const openDrawerHandler = () => {
+      setDrawerIsOpen(true);
+    };
+  
+    const closeDrawerHandler = () => {
+      setDrawerIsOpen(false);
+    };
+  
+    return (
+      <React.Fragment>
+        {drawerIsOpen && <Backdrop onClick={closeDrawerHandler} />}
+        <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
+          <nav className="main-navigation__drawer-nav">
+            <NavLinks />
+          </nav>
+        </SideDrawer>
+  
+        <MainHeader>
+          <button className="main-navigation__menu-btn" onClick={openDrawerHandler}>
+            <span />
+            <span />
+            <span />
+          </button>
+          <h1 className="main-navigation__title">
+            <Link to="/">YourPlaces</Link>
+          </h1>
+          <nav className="main-navigation__header-nav">
+            <NavLinks />
+          </nav>
+        </MainHeader>
+      </React.Fragment>
+    );
+  };
+  
+  export default MainNavigation;
+  
