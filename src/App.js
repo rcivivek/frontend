8f4327a8de0e4bbcd6f01710ea07461744fd0128
Vivek Goyal <rcivivek41214008@gmail.com>
2023-07-09 06:27:46 +0530

    Dummy User profile is done

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import React from 'react';
 import { BrowserRouter  as Router,Route}  from 'react-router-dom/';
 
 import Users from './user/pages/Users';
-import { Redirect } from 'react-router-dom/cjs/react-router-dom.min';
+import NewPlace  from './places/pages/NewPlace';
+import { Redirect ,Switch} from 'react-router-dom/cjs/react-router-dom.min';
 
 const  App=() =>{
   return <Router>
+    <Switch>
     <Route path='/' exact>
        <Users />
     </Route>
+    <Route path='/places/new' exact>
+       <NewPlace />
+    </Route>
     <Redirect to='/'/>
+    </Switch>
     </Router>
 }
 
