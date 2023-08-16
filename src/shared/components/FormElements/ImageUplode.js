f0d4ebb7c65dd92c7790880b64a310cae9d9aa38
Vivek Goyal <rcivivek41214008@gmail.com>
2023-08-17 00:08:19 +0530

    FormData is used instead of Json data to send photo.

--- src/shared/components/FormElements/ImageUplode.js
+++ src/shared/components/FormElements/ImageUplode.js
@@ -1,66 +1,67 @@
-import React, {useRef,useState, useEffect} from "react";
-import Button from "./Button";
-import './ImageUpload.css'
+import React, { useRef, useState, useEffect } from 'react';
 
-const ImageUpload = props =>{
-     const filePickerRef =useRef();
-     const [file, setFile] = useState();
-     const [previewUrl, setPreveiwUrl] = useState();
-     const [isValid, setIsValid]  = useState(false);
+import Button from './Button';
+import './ImageUpload.css';
 
-     useEffect(()=>{
-        if(!file){
-            return;
+const ImageUpload = props => {
+  const [file, setFile] = useState();
+  const [previewUrl, setPreviewUrl] = useState();
+  const [isValid, setIsValid] = useState(false);
 
-        }
-        const fileReader = new FileReader();
-        fileReader.onload = () => {
-            setPreveiwUrl(fileReader.result);
-        };
-        fileReader.readAsDataURL(file);
-     },[file]);
-     
-     const pickedHandler = event =>{
-        let pickedFile;
-        let fileIsValid = isValid;
-        if(event.target.files && event.target.files.length ===1){
-             pickedFile = event.target.files[0];
-            setFile(pickedFile);
-            setIsValid(true);   
-            fileIsValid = true;  
-           }else{
-            setIsValid(false); 
-            fileIsValid = false;
-        }
-        props.onInput(props.id,pickedFile,isValid);
-     }
+  const filePickerRef = useRef();
 
-     const pickImageHandler = () => {
-       filePickerRef.current.click();
-     };
+  useEffect(() => {
+    if (!file) {
+      return;
+    }
+    const fileReader = new FileReader();
+    fileReader.onload = () => {
+      setPreviewUrl(fileReader.result);
+    };
+    fileReader.readAsDataURL(file);
+  }, [file]);
 
-    return (
-        <div className="form-control">
-            <input 
-             id = {props.id} 
-             ref = {filePickerRef}
-             style={{display:'none'}} 
-             type="file" 
-             accept=".jpg,.png, .jpeg"
-             onChange = {pickedHandler}  
-             />
-             <div className={`image-upload ${props.center && 'center'}`} >
-                <div className="image-upload__preview">
-                   { previewUrl && <img src={previewUrl} alt="Preview" /> }
-                   { !previewUrl &&<p>Please pick a image</p>}
-                </div>
-                <Button type="button"  onClick ={pickImageHandler} >PICK IMAGE</Button>
-             </div>
-             {!isValid && <p>{props.errorText}</p>}
+  const pickedHandler = event => {
+    let pickedFile;
+    let fileIsValid = isValid;
+    if (event.target.files && event.target.files.length === 1) {
+      pickedFile = event.target.files[0];
+      setFile(pickedFile);
+      setIsValid(true);
+      fileIsValid = true;
+    } else {
+      setIsValid(false);
+      fileIsValid = false;
+    }
+    props.onInput(props.id, pickedFile, fileIsValid);
+  };
+
+  const pickImageHandler = () => {
+    filePickerRef.current.click();
+  };
+
+  return (
+    <div className="form-control">
+      <input
+        id={props.id}
+        ref={filePickerRef}
+        style={{ display: 'none' }}
+        type="file"
+        accept=".jpg,.png,.jpeg"
+        onChange={pickedHandler}
+      />
+      <div className={`image-upload ${props.center && 'center'}`}>
+        <div className="image-upload__preview">
+          {previewUrl && <img src={previewUrl} alt="Preview" />}
+          {!previewUrl && <p>Please pick an image.</p>}
         </div>
-
-    );
-
+        <Button type="button" onClick={pickImageHandler}>
+          PICK IMAGE
+        </Button>
+      </div>
+      {!isValid && <p>{props.errorText}</p>}
+    </div>
+  );
 };
 
 export default ImageUpload;
